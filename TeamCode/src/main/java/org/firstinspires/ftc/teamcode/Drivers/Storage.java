7bd17455125f8cc6b4ac69b4276fdeb935d956f0
Tyler Wang <wangtyler123@gmail.com>
2020-12-28 17:33:49 -0600

    Set up a constructor to be implemented.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Storage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Storage.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode.Drivers;
 
+import com.qualcomm.robotcore.hardware.Servo;
+
 import org.firstinspires.ftc.teamcode.RobotFunctions.RobotStorage;
 
 public class Storage extends RobotStorage {
+    public Storage(Servo servo, double maxAngle) {
+        //todo implement a constructor
+    }
     @Override
     protected void setPosition(int postion) {
         //todo make a function that sets the position of the feeder.
