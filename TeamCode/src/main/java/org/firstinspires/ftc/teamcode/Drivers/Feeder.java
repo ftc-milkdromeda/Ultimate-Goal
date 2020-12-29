9a105bb271153fbb7c47a055d73bdfc5da90ff09
Alex Wang <aloyfusion@gmail.com>
2020-12-28 20:36:52 -0600

    Completed the functions but needs the correct servo values.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Feeder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Feeder.java
@@ -5,13 +5,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.RobotFunctions.RobotFeeder;
 import org.firstinspires.ftc.teamcode.RobotFunctions.RobotStorage;
 
+import java.nio.file.attribute.UserDefinedFileAttributeView;
+
 public class Feeder extends RobotFeeder {
     public Feeder(RobotStorage storage, Servo servo, double maxAngle) {
         super(storage);
+        this.servo = servo;
     }
 
     @Override
     protected void feed() {
-        //todo implement a feed function
+        servo.setPosition(0.0);
+        servo.setPosition(0.0);
     }
+    private Servo servo;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Storage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Storage.java
@@ -5,11 +5,38 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.RobotFunctions.RobotStorage;
 
 public class Storage extends RobotStorage {
-    public Storage(Servo servo, double maxAngle) {
-        //todo implement a constructor
+    public Storage(Servo liftServo, Servo storageServo, double maxAngle) {
+        super();
+        this.liftServo = liftServo;
+        this.storageServo = storageServo;
+        this.maxAngle = maxAngle;
     }
+
     @Override
-    protected void setPosition(int postion) {
-        //todo make a function that sets the position of the feeder.
+    protected void setPosition(int position) {
+        if (position == 0) {
+           liftServo.setPosition(0.0);
+           storageServo.setPosition(0.0);
+        }
+
+
+        else if (position == 1) {
+            liftServo.setPosition(0.0);
+            storageServo.setPosition(0.0);
+        }
+
+        else if (position == 2) {
+            liftServo.setPosition(0.0);
+            storageServo.setPosition(0.0);
+        }
+
+        else if (position ==3) {
+            liftServo.setPosition(0.0);
+            storageServo.setPosition(0.0);
+        }
     }
+
+    private Servo liftServo;
+    private Servo storageServo;
+    private double maxAngle;
 }
