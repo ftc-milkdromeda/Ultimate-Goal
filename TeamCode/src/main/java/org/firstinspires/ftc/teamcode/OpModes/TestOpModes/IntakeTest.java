0a03149f1c8d85c851f02b15b0b11695a05cde42
Tyler Wang <wangtyler123@gmail.com>
2020-12-29 13:09:39 -0600

    Created new code to test intake motor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestOpModes/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestOpModes/IntakeTest.java
@@ -1,71 +1,68 @@
 package org.firstinspires.ftc.teamcode.OpModes.TestOpModes;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-//@Disabled
-@TeleOp(name="Intake Test")
-public class IntakeTest extends LinearOpMode {
+public class IntakeTest extends OpMode {
     @Override
-    public void runOpMode() {
+    public void init() {
         this.intakeMotor = hardwareMap.dcMotor.get("intake");
-        this.intakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
         this.power = 1;
+    }
 
+    @Override
+    public void loop() {
         final double bigIncrement = 0.05;
         final double smallIncrement = 0.0005;
 
-        this.waitForStart();
-
-        while(super.opModeIsActive()) {
-            while (!this.status) {
-                if (super.gamepad1.b && this.power <= 1 - bigIncrement) {
-                    this.power += bigIncrement;
-                    while (super.gamepad1.b) {
-                        super.telemetry.addData("Value: ", "%.5f", this.power);
-                        super.telemetry.update();
-                    }
-                }
-                else if (super.gamepad1.a && this.power >= bigIncrement) {
-                    this.power -= bigIncrement;
-                    while (super.gamepad1.a) {
-                        super.telemetry.addData("Value: ", "%.5f", this.power);
-                        super.telemetry.update();
-                    }
-                }
-                else if (super.gamepad1.x && this.power >= smallIncrement) {
-                    this.power -= smallIncrement;
-                    while (super.gamepad1.x) {
-                        super.telemetry.addData("Value: ", "%.5f", this.power);
-                        super.telemetry.update();
-                    }
-                }
-                else if (super.gamepad1.y && this.power <= 1 - smallIncrement) {
-                    this.power += smallIncrement;
-                    while (super.gamepad1.y) {
-                        super.telemetry.addData("Value: ", "%.5f", this.power);
-                        super.telemetry.update();
-                    }
-                }
-                else if (super.gamepad1.left_bumper) {
-                    this.status = true;
-                    while (super.gamepad1.left_bumper) {
-                        super.telemetry.addData("Value: ", "%.5f", this.power);
-                        super.telemetry.update();
-                    }
+        while(!this.status) {
+            if (super.gamepad1.b && this.power <= 1 - bigIncrement) {
+                this.power += bigIncrement;
+                while (super.gamepad1.b) {
+                    super.telemetry.addData("Value: ", "%.5f", this.power);
+                    super.telemetry.update();
                 }
             }
+            else if (super.gamepad1.a && this.power >= bigIncrement) {
+                this.power -= bigIncrement;
+                while (super.gamepad1.a) {
+                    super.telemetry.addData("Value: ", "%.5f", this.power);
+                    super.telemetry.update();
+                }
+            }
+            else if (super.gamepad1.x && this.power >= smallIncrement) {
+                this.power -= smallIncrement;
+                while (super.gamepad1.x) {
+                    super.telemetry.addData("Value: ", "%.5f", this.power);
+                    super.telemetry.update();
+                }
+            }
+            else if (super.gamepad1.y && this.power <= 1 - smallIncrement) {
+                this.power += smallIncrement;
+                while (super.gamepad1.y) {
+                    super.telemetry.addData("Value: ", "%.5f", this.power);
+                    super.telemetry.update();
+                }
+            }
+            else if(super.gamepad1.left_bumper) {
+                this.status = true;
+                while (super.gamepad1.left_bumper) {
+                    super.telemetry.addData("Value: ", "%.5f", this.power);
+                    super.telemetry.update();
+                }
+            }
+        }
 
-            this.intakeMotor.setPower(this.power);
+        this.intakeMotor.setPower(this.power);
 
-            while (this.status)
-                if (super.gamepad1.right_bumper)
-                    this.status = false;
-
-            this.intakeMotor.setPower(0);
+        while(this.status) {
+            if(super.gamepad1.left_bumper) {
+                this.status = false;
+                while (super.gamepad1.left_bumper) {
+                    super.telemetry.addData("Value: ", "%.5f", this.power);
+                    super.telemetry.update();
+                }
+            }
         }
     }
 
