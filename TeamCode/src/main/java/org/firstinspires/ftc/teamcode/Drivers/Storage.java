63031f61f2e562edfaf5e17f00659f446999df15
Alex Wang <aloyfusion@gmail.com>
2020-12-29 19:14:30 -0600

    Updated values to constant variables.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Storage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Storage.java
@@ -8,32 +8,40 @@ public class Storage extends RobotStorage {
     public Storage(Servo liftServo, Servo storageServo) {
         super();
         this.liftServo = liftServo; //Lifts the storage up and down.
-        this.storageServo = storageServo; //Rotates the storage box to line up with the shooter box.
+        this.storageServo = storageServo; //Rotates the storage box to line it up with the shooter box.
 
-        this.liftServo.setPosition(0.0f);
-        this.storageServo.setPosition(1.0f);
+        this.liftServo.setPosition(Storage.liftInitialPosition);
+        this.storageServo.setPosition(Storage.storageInitialPosition);
     }
 
     @Override
     protected void setPosition(int position) {
         if (position == 0) {
-           liftServo.setPosition(0.0);
-           storageServo.setPosition(1.0);
+           liftServo.setPosition(liftInitialPosition);
+           storageServo.setPosition(storageInitialPosition);
         }
         else if (position == 1) {
-            liftServo.setPosition(0.0);
-            storageServo.setPosition(0.9);
+            liftServo.setPosition(liftPos1);
+            storageServo.setPosition(storageInitialPosition);
         }
         else if (position == 2) {
-            liftServo.setPosition(0.635);
-            storageServo.setPosition(0.9);
+            liftServo.setPosition(liftPos2);
+            storageServo.setPosition(storageExtendedPosition);
         }
         else if (position == 3) {
-            liftServo.setPosition(0.8985);
-            storageServo.setPosition(0.9);
+            liftServo.setPosition(liftPos3);
+            storageServo.setPosition(storageExtendedPosition);
         }
     }
 
     private Servo liftServo;
     private Servo storageServo;
+
+    //constants
+    private static final double liftInitialPosition = 0.0;
+    private static final double liftPos1 = 0.0;
+    private static final double liftPos2 = 0.625;
+    private static final double liftPos3 = 0.8985;
+    private static final double storageInitialPosition = 1.0;
+    private static final double storageExtendedPosition = 0.9;
 }
