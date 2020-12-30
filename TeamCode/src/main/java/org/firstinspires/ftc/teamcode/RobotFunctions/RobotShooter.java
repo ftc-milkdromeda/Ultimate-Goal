007d6e82706f26a244c393ea30aaddc713fd2135
Tyler Wang <wangtyler123@gmail.com>
2020-12-30 15:31:13 -0600

    Added new shooting function to robot.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/RobotShooter.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.RobotFunctions;
+
+import RobotFunctions.Units_length;
+
+public abstract class RobotShooter {
+    public abstract void runShooter(double distance, Units_length units);
+}
