e90dd8e9343c7ace68e1fba6b7ded63ae978a850
Alex Wang <aloyfusion@gmail.com>
2021-01-02 16:35:01 -0600

    Fixed Values.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Feeder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Feeder.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.Drivers;
 
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.RobotFunctions.RobotFeeder;
@@ -9,27 +8,22 @@ import org.firstinspires.ftc.teamcode.RobotFunctions.RobotStorage;
 import java.nio.file.attribute.UserDefinedFileAttributeView;
 
 public class Feeder extends RobotFeeder {
-    public Feeder(RobotStorage storage, HardwareMap hardware) {
+    public Feeder(RobotStorage storage, Servo servo) {
         super(storage);
-
-        this.servo = hardware.servo.get("feeder");
+        this.servo = servo;
         this.servo.setPosition(Feeder.initialPosition);
     }
 
     @Override
     protected void feed() {
+        servo.setPosition(Feeder.initialPosition);
         servo.setPosition(Feeder.extendedPosition);
-        long startTime = System.currentTimeMillis();
-
-        while(System.currentTimeMillis() - startTime <= Feeder.timeExtended);
-
         servo.setPosition(Feeder.initialPosition);
     }
 
     private Servo servo;
 
     //constants
-    private static final double initialPosition = 0.975;
-    private static final double extendedPosition = 0.8;
-    private static final int timeExtended = 150;
+    private static final double initialPosition = 0.97;
+    private static final double extendedPosition = 0.75;
 }
