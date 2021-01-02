4274b8a6ea6c4ced7e416424c9c379173ddd3552
Alex Wang <aloyfusion@gmail.com>
2021-01-02 16:35:01 -0600

    fixed merge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Storage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Storage.java
@@ -39,9 +39,9 @@ public class Storage extends RobotStorage {
 
     //constants
     private static final double liftInitialPosition = 0.0;
-    private static final double liftPos1 = 0.0;
-    private static final double liftPos2 = 0.725;
-    private static final double liftPos3 = 1.0;
+    private static final double liftPos1 = 0.1;
+    private static final double liftPos2 = 0.7;
+    private static final double liftPos3 = 1.95;
     private static final double storageInitialPosition = 1.0;
-    private static final double storageExtendedPosition = 0.885;
+    private static final double storageExtendedPosition = 0.905;
 }
