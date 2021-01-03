de1d72fcdf34c43d0c82d157878ed27445cf21e3
Alex Wang <aloyfusion@gmail.com>
2021-01-03 00:29:45 -0600

    Made intake driver.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Intake.java
@@ -1,34 +1,18 @@
 package org.firstinspires.ftc.teamcode.Drivers;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.RobotFunctions.RobotIntake;
 
 public class Intake extends RobotIntake {
-    public Intake(Storage storage, HardwareMap hardware) {
+    public Intake(HardwareMap hardware) {
         super();
 
-        this.storage = storage;
-        this.intake = hardware.dcMotor.get("intake");
-        this.intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
     }
 
     @Override
     public void runIntake() {
-        this.intake.setPower(Intake.power);
-        this.storage.shake();
+
     }
-
-    @Override
-    public void stopIntake() {
-        this.intake.setPower(0.0);
-        this.storage.shakeEnd();
-    }
-
-    private DcMotor intake;
-    private Storage storage;
-
-    //constants
-    private static final double power = 1.0;
 }
