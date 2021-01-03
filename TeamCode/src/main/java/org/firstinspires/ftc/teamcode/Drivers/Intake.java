b2cba4cb43e5811564d8598582dded574a69d525
Alex Wang <aloyfusion@gmail.com>
2021-01-03 00:29:45 -0600

    Made intake driver.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Intake.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.Drivers;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.RobotFunctions.RobotIntake;
+
+public class Intake extends RobotIntake {
+    public Intake(HardwareMap hardware) {
+        super();
+
+
+    }
+
+    @Override
+    public void runIntake() {
+
+    }
+}
