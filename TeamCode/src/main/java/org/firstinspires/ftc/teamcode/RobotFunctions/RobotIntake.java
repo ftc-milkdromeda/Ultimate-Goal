964d62a04f589515309a9af3ba924e89a4216bee
Alex Wang <aloyfusion@gmail.com>
2021-01-03 17:34:36 -0600

    Added intake feature to robot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/RobotIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/RobotIntake.java
@@ -2,5 +2,6 @@ package org.firstinspires.ftc.teamcode.RobotFunctions;
 
 public abstract class RobotIntake {
     public abstract void runIntake();
+    public abstract void stopIntake();
 }
 //todo make subclass that interfaces motors to work with the intake.
