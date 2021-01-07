40012aeb3c9d2dc8d9b7ee32248928ada7dfebb7
Alex Wang <aloyfusion@gmail.com>
2021-01-06 20:55:14 -0600

    Changed stopIntake to reverseIntake.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivers/Intake.java
@@ -5,50 +5,32 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.RobotFunctions.RobotIntake;
 
-public class Intake extends RobotIntake {
+public class Intake extends RobotIntake implements Drivers{
     public Intake(Storage storage, HardwareMap hardware) {
         super();
 
         this.storage = storage;
         this.intake = hardware.dcMotor.get("intake");
         this.intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        this.status = false;
     }
 
     @Override
     public void runIntake() {
-        if(this.status)
-            return;
-        this.status = true;
-
         this.intake.setPower(Intake.power);
         this.storage.shake();
     }
 
     @Override
-    public void stopIntake() {
-        if(!this.status)
-                return;
-
-        this.intake.setPower(0.0);
-        this.storage.shakeEnd();
-        this.status = false;
-    }
-
-    @Override
-    public void hardStop() {
-        this.intake.setPower(0.0);
+    public void reverseIntake() {
+        this.intake.setPower(-0.25);
         this.storage.shakeEnd();
     }
 
     @Override
-    public void terminate() {
-        this.stopIntake();
-    }
+    public void destructor() {}
 
     private DcMotor intake;
     private Storage storage;
-    private boolean status;
 
     //constants
     private static final double power = 1.0;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/RobotIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/RobotIntake.java
@@ -1,15 +1,12 @@
 package org.firstinspires.ftc.teamcode.RobotFunctions;
 
-import Drivers.Template.Driver;
-
-public abstract class RobotIntake extends Driver {
+public abstract class RobotIntake {
     protected  RobotIntake() {
         this.busy = false;
     }
 
     public abstract void runIntake();
-    public abstract void stopIntake();
-    public abstract void hardStop();
+    public abstract void reverseIntake();
 
     public boolean isBusy() {
         return this.busy;
