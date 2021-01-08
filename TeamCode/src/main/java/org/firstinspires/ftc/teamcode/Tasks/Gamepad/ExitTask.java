f6cc8dcf2a84ea6a134895b37c45a852351b925f
Tyler Wang <wangtyler123@gmail.com>
2021-01-08 00:03:34 -0600

    Added task that produces an alternate exit button.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tasks/Gamepad/ExitTask.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.Tasks.Gamepad;
+
+import Drivers.Controller;
+import TaskManager.Clock;
+import TaskManager.KeyTask;
+
+public class ExitTask extends KeyTask {
+    public ExitTask(Controller controller) {
+        super(null, controller);
+
+        this.exit = false;
+    }
+
+    @Override
+    protected double[] keyMapping() {
+        double returnArray[] = {super.controller.get_Back()};
+        return returnArray;
+    }
+
+    @Override
+    public void run() {
+        while(!super.isInterrupted()) {
+            if(this.keyMapping()[0] == 1.0)
+                break;
+        }
+        this.exit = true;
+    }
+
+    public boolean getExit() {
+        return this.exit;
+    }
+
+    private boolean exit;
+}
