eb12cae49fc8db2681cb392344093f17ae994869
Tyler Wang <wangtyler123@gmail.com>
2021-01-09 17:49:06 -0600

    Updated classes to suite new Driver class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/RobotStorage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/RobotStorage.java
@@ -1,10 +1,17 @@
 package org.firstinspires.ftc.teamcode.RobotFunctions;
 
+import Drivers.DriverManager;
 import Drivers.Template.Driver;
+import TaskManager.Task;
 
 public abstract class RobotStorage extends Driver {
     protected RobotStorage() {
-        this.busy = false;
+        super();
+
+        if(RobotStorage.processId != -1)
+            super.alive = false;
+        else
+            RobotStorage.processId = DriverManager.attachProcess(this);
     }
 
     /**
@@ -12,38 +19,38 @@ public abstract class RobotStorage extends Driver {
      * @param postion An integer 0 - 3, where 0 is the intake position; where 1 - 3 is each of its shooting positions.
      * @return returns true on success; false if function failed to execute.
      */
-    public abstract boolean setPosition(int postion);
-    protected abstract boolean shake();
-    protected abstract boolean shakeEnd();
+    public abstract boolean setPosition(Task task, int postion);
+    public abstract boolean shake(Task task);
+    public abstract boolean shakeEnd(Task task);
 
     public int getRings() {
-        return this.numOfRings;
+        return RobotStorage.numOfRings;
     }
     public void setRings(int rings) {
-        this.numOfRings = rings;
+        RobotStorage.numOfRings = rings;
     }
     public void addRing() {
-        this.numOfRings++;
+        RobotStorage.numOfRings++;
     }
     public void removeRing() {
-        this.numOfRings--;
+        RobotStorage.numOfRings--;
     }
 
-    public boolean nextRing() {
-        if(numOfRings == 0)
+    public boolean nextRing(Task task) {
+        if(numOfRings == 0 || super.testTask(task))
             return false;
 
-        this.setPosition(4 - numOfRings);
+        this.setPosition(task, 4 - numOfRings);
 
         return true;
     }
 
-    public boolean isBusy() {
-        return this.busy;
+    @Override
+    protected void destructor() {
+        RobotStorage.processId = -1;
     }
 
-    private int position;
-    private int numOfRings;
-
-    protected boolean busy;
+    private static int position;
+    private static int numOfRings;
+    private static int processId = -1;
 }
