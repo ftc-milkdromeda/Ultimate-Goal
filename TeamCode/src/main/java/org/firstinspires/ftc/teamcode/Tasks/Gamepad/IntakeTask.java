c7f510c5287cfa10377a1a00bcd5288498301cfb
Tyler Wang <wangtyler123@gmail.com>
2021-01-11 18:49:52 -0600

    Added dump function.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tasks/Gamepad/IntakeTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tasks/Gamepad/IntakeTask.java
@@ -1,13 +1,15 @@
 package org.firstinspires.ftc.teamcode.Tasks.Gamepad;
 
 import org.firstinspires.ftc.teamcode.RobotFunctions.RobotIntake;
+import org.firstinspires.ftc.teamcode.RobotFunctions.RobotStorage;
 
 import TaskManager.Clock;
-import TaskManager.Task;
+import TaskManager.KeyTask;
+import Drivers.Controller;
 
-public class IntakeTask extends Task {
-    protected IntakeTask(Clock clock, RobotIntake intake) {
-        super(clock);
+public class IntakeTask extends KeyTask {
+    protected IntakeTask(Clock clock, Controller controller, RobotIntake intake, RobotStorage storage) {
+        super(clock, controller);
 
         this.intake = intake;
         this.isRunning = false;
@@ -42,10 +44,29 @@ public class IntakeTask extends Task {
         this.isActive = true;
         IntakeTask.status = true;
 
-        while(!super.isInterrupted());
+        while(!super.isInterrupted()) {
+            while(!this.isRunning);
+
+            if(this.keyMapping()[0] == 1.0) {
+                this.storage.shakeEnd(this);
+                this.storage.dump(this);
+            }
+
+            int startClock = super.clock.getCurrentState();
+            while(super.clock.getCurrentState() == startClock);
+        }
 
         IntakeTask.status = false;
         this.isActive = false;
+    }
+
+    @Override
+    protected double[] keyMapping() {
+        double returnArray[] = {
+                super.controller.get_B()
+        };
+
+        return returnArray;
     }
 
     @Override
@@ -57,6 +78,7 @@ public class IntakeTask extends Task {
     private static boolean status = false;
 
     private RobotIntake intake;
+    private RobotStorage storage;
     private boolean isRunning;
     private boolean isActive;
 }
